a3ea98de681ec67502fe0b15cda683d790889ca2
JihyeSeo1120 <mansesjh22@gmail.com>
2021-09-30 01:15:33 +0900

    Sass - 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div className="App"></div>;
+import { Component } from "react";
+import SassComponent from "./SassComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
